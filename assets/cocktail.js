075e9d2646549828f9e5e90ea58a5f3fd0e06128
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-10-25 15:05:30 -0500

    having trouble w/ last part of API srry for on main

--- assets/cocktail.js
+++ assets/cocktail.js
@@ -27,8 +27,32 @@ function getRandomDrink() {
     console.log(cocktail.drinks[0])
 
     let drinkSection = document.querySelector('#drink-section');
+    // could probably get elements by class or id and they would be inside the card...
     let DrinkName = document.createElement('h3');
     DrinkName.innerHTML = cocktail.drinks[0].strDrink;
 
     drinkSection.appendChild(DrinkName);
+
+    let img = document.createElement('img');
+    img.src = cocktail.drinks[0].strDrinkThumb;
+
+    drinkSection.appendChild(img);
+
+    for (let i = 1; i < 16; i++){
+      console.log();
+
+      // if (cocktail.drinks[0]['strMeasure${i}'] == null){
+      //   break;
+      // }
+
+      let ingredient = document.createElement('on-list-item');
+      ingredient.innerHTML = cocktail.drinks[0]['strMeasure${i}'] + ': ' + cocktail.drinks[0]['strIngredient${i}'];
+
+      drinkSection.appendChild(ingredient);
+    }
+    let card = document.createElement('on-card');
+    card.innderHTML = cocktail.drinks[0].strInstructions;
+
+    drinkSection.appendChild(card);
+
   }
