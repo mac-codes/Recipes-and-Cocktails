988f36950bd21c48f520a66d305b041fa72e262a
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-10-24 21:13:38 -0500

    merging api issue fixed

--- assets/cocktail.js
+++ assets/cocktail.js
@@ -1,14 +1,6 @@
 
-const xhr = new XMLHttpRequest();
-const url = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
-
-xhr.open("GET", url);
-xhr.onreadystatechange = url;
-xhr.send();
-
-
 function getRandomDrink() {
-  fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(
     function(response) {
         if (response.status !== 200) {
@@ -32,5 +24,11 @@ function getRandomDrink() {
   getRandomDrink();
 
   function displayRandomDrink(cocktail){
-    console.log(cocktail.drinks[0].strDrink)
+    console.log(cocktail.drinks[0])
+
+    let drinkSection = document.querySelector('#drink-section');
+    let DrinkName = document.createElement('h3');
+    DrinkName.innerHTML = cocktail.drinks[0].strDrink;
+
+    drinkSection.appendChild(DrinkName);
   }
