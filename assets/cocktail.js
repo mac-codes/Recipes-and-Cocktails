5bb27b5e0ef907d69a28b5f562e4f63a5cfa8020
McKenna Lawson <mckenna.lawson01@gmail.com>
2022-10-25 21:22:37 -0500

    merge

--- assets/cocktail.js
+++ assets/cocktail.js
@@ -2,56 +2,55 @@
 function getRandomDrink() {
   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(
-    function(response) {
+      function (response) {
         if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
+          console.log('Looks like there was a problem. Status Code: ' +
             response.status);
-        return;
+          return;
         }
 
         // Examine the text in the response
-        response.json().then(function(data) {
-        console.log(data);
-        displayRandomDrink(data)
+        response.json().then(function (data) {
+          console.log(data);
+          displayRandomDrink(data)
         });
-    }
-    )
-    .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-    });
-  }
-
-  getRandomDrink();
-
-  function displayRandomDrink(cocktail){
-    console.log(cocktail.drinks[0])
-
-    let drinkSection = document.querySelector('#drink-section');
-    // could probably get elements by class or id and they would be inside the card...
-    let drinkName = document.getElementById('drink-name');
-    drinkName.textContent = cocktail.drinks[0].strDrink;
-    
-
-    let img = document.getElementById('drink-image');
-    img.setAttribute('src', cocktail.drinks[0].strDrinkThumb);
-
-
-    for (let i = 1; i < 16; i++){
-      console.log();
-
-      if (cocktail.drinks[0][`strMeasure${i}`] === null){
-        break;
-      } if (cocktail.drinks[0][`strIngredient${i}`] === null) {
-        break;
       }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
 
-      let ingredient = document.getElementById('on-list-item');
-      ingredient.textContent = cocktail.drinks[0][`strMeasure${i}`] + ': ' + cocktail.drinks[0][`strIngredient${i}`];
+getRandomDrink();
 
+function displayRandomDrink(cocktail) {
+  console.log(cocktail.drinks[0])
+
+  let drinkSection = document.querySelector('#drink-section');
+  // could probably get elements by class or id and they would be inside the card...
+  let drinkName = document.getElementById('drink-name');
+  drinkName.textContent = cocktail.drinks[0].strDrink;
+
+
+  let img = document.getElementById('drink-image');
+  img.setAttribute('src', cocktail.drinks[0].strDrinkThumb);
+
+
+  for (let i = 1; i < 16; i++) {
+    console.log();
+
+    if (cocktail.drinks[0][`strMeasure${i}`] === null) {
+      break;
+    } if (cocktail.drinks[0][`strIngredient${i}`] === null) {
+      break;
     }
-    let card = document.createElement('on-card');
-    card.innderHTML = cocktail.drinks[0].strInstructions;
 
-    drinkSection.appendChild(card);
+    let ingredient = document.getElementById('on-list-item');
+    ingredient.textContent = cocktail.drinks[0][`strMeasure${i}`] + ': ' + cocktail.drinks[0][`strIngredient${i}`];
 
   }
+  let card = document.getElementById('on-card');
+  card.textContent = cocktail.drinks[0].strInstructions;
+
+
+}
